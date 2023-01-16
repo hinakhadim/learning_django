2e4e3ac37c99e33db41d4922841f80ee07f2c1cb
Hina Khadim <hina.khadim@edly.io>
2023-01-16 19:25:04 +0500

    test: testing gpg work or not

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['question_text', 'pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,14 +10,16 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return {self.question_text}
+        return self.question_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,
+                                 related_name='choices'
+                                 )
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
